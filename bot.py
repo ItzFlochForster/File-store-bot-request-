9803aef5e791d13ee75fa3edf75b651146262fbd
ItzFlochForster <Meowtaro6659@gmail.com>
2025-09-21 17:46:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -98,9 +98,8 @@ class Bot(Client):
         # web-response
         app = web.AppRunner(await web_server())
         await app.setup()
-        bind_address = "0.0.0.0"
-        await web.TCPSite(app, bind_address, PORT).start()
 
+    
     async def stop(self, *args):
         await super().stop()
         self.LOGGER(__name__).info("Bot stopped.")
